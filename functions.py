2ef40bab2698f577c5b19e8ec7be214b5dfd4a29
bmarid <mary.baj@yandex.ru>
2023-04-14 01:51:18 +0200

    add lib and func

---
+++ functions.py
@@ -0,0 +1,36 @@
+from libs import *
+
+# getting special name of column in df
+def get_choice(cases_or_deaths, data_type, data_type_choices):
+    # st.write("get_choice")
+    # st.write("Cases or? - ",cases_or_deaths)
+    # st.write("Type - ",data_type)
+    # st.write()
+    choice = ''
+
+    if cases_or_deaths == 'cases':
+        choice = 'cases'
+    elif cases_or_deaths == 'deaths':
+        choice = 'deaths'
+
+    column = ''
+    
+    if data_type == data_type_choices[0]: #'raw number':
+        column = 'new_'+ choice +'_per_million'
+    elif data_type == data_type_choices[1]: #'cumulative number':
+        column = 'cumulative_' + choice
+    elif data_type == data_type_choices[2]: #'average - 7 days':
+        column = 'average_' + choice
+
+    # st.write("choise! - ",choice)
+    # st.write("column - ",column)
+    
+    return (choice, column)
+
+
+def get_peaks(filtered_df, column, cases_or_deaths):
+    peaks, _ = find_peaks(filtered_df[column])
+    peak_column = 'peak_' + cases_or_deaths
+    filtered_df[peak_column] = np.nan
+    filtered_df.loc[peaks, peak_column] = filtered_df.loc[peaks, column]
+    return filtered_df
---
+++ libs.py
@@ -0,0 +1,16 @@
+#streamlit
+import streamlit as st
+
+#data dependencies 
+import pandas as pd
+import plotly.express as px
+import warnings
+warnings.filterwarnings("ignore")
+from datetime import date
+
+import numpy as np
+from datetime import datetime
+from datetime import date
+
+from scipy.signal import find_peaks
+import matplotlib.pyplot as plt
