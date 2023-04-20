4f803e83e30b37be9c41f7a497cdfd65371d22ce
[tiffany <tiffany2626lin@gmail.com>
2023-04-20 13:29:46 +0200

    issue #121 - moved cases/deaths + data type dropdowns above graph1

--- covid_dashboard.py
+++ covid_dashboard.py
@@ -111,7 +111,7 @@ data_load_state = st.text('Loading data...')
 #get final df
 df_final = get_final_df(df, transform_cols)
 
-data_load_state.text("Done with loading!)")
+data_load_state.text("Done with loading!")
 
 #get contries and continent
 countries = sorted(df_final['location'].unique())
@@ -125,18 +125,7 @@ if st.checkbox('Show raw data'):
     st.write(df_final)
 
 # SIDEBAR
-# select cases or deaths
 st.sidebar.title(":mag_right: View Options:")
-cases_or_deaths_choices = ['Cases', 'Deaths']
-cases_or_deaths = st.sidebar.selectbox("View cases or deaths",cases_or_deaths_choices)
-
-# select data type
-data_type_choices = ['Raw number', 'Cumulative number', 'Average - 7 days']
-data_type = st.sidebar.selectbox("View Data type", data_type_choices)
-
-if data_type == data_type_choices[1]: #'cumulative number'
-    # select to show peaks
-    show_peaks = st.sidebar.checkbox("Show peaks")
 
 #select countries or continent
 show_by=st.sidebar.radio(
@@ -173,9 +162,29 @@ elif show_by=="Continent":
 st.header(":mask: Covid-19 Data")
 
 #add time double_ended_slider
-values = st.slider(
-    'Select a date range: ',
-    min_value=min_date,max_value=max_date, value=(date(2021,5,7),date(2022,4,7)),step=timedelta(days=1))
+#values = st.slider(
+#    'Select a date range: ',
+#    min_value=min_date,max_value=max_date, value=(date(2021,5,7),date(2022,4,7)),step=timedelta(days=1))
+
+#filtered_graph1=filtered_place_graph1[(filtered_place_graph1['day'] >= values[0]) & (filtered_place_graph1['day']<= values[1])]
+
+# select cases or deaths and data type (raw number, cumulative, average 7 days)
+cases_or_deaths, data_type,= st.columns([5, 5])
+with cases_or_deaths:
+    cases_or_deaths_choices = ['Cases', 'Deaths']
+    cases_or_deaths = st.selectbox("View cases or deaths",cases_or_deaths_choices)
+    
+with data_type:
+    data_type_choices = ['Raw number', 'Cumulative number', 'Average - 7 days']
+    data_type = st.selectbox("View Data type", data_type_choices)
+
+
+if data_type == data_type_choices[1]: #'cumulative number'
+    # select to show peaks
+    show_peaks = st.sidebar.checkbox("Show peaks")
+
+#add time double_ended_slider
+values = st.slider('Select a date range: ',min_value=min_date,max_value=max_date, value=(date(2021,5,7),date(2022,4,7)),step=timedelta(days=1))
 
 filtered_graph1=filtered_place_graph1[(filtered_place_graph1['day'] >= values[0]) & (filtered_place_graph1['day']<= values[1])]
 
